b83b799a66f0d58cdfc1e4676b63fc25933462d6
Mauro M <hello@maurom.dev>
2023-09-07 09:11:52 +0100

    feat: add sentry

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import './assets/index.css'
-
 import { createApp } from 'vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import LandingView from '@/views/LandingView.vue'
 import ChartsView from '@/views/ChartsView.vue'
+import * as Sentry from '@sentry/vue'
+import { HttpClient } from '@sentry/integrations'
+
 const routes = [
   { path: '/', component: LandingView },
   { path: '/charts', component: ChartsView }
@@ -15,6 +17,21 @@ const router = createRouter({
 })
 
 const app = createApp({})
+Sentry.init({
+  app,
+  dsn: import.meta.env.VITE_SENTRY_URI,
+  trackComponents: true,
+  integrations: [
+    new Sentry.BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router)
+    }),
+    new Sentry.Replay(),
+    new HttpClient()
+  ],
+  tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0
+})
 app.use(router)
 
 app.mount('#app')
